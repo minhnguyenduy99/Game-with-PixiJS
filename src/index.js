d5c5047c6e766f30facdf3d51786e659165cab18
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-03-24 17:23:16 +0700

    add bump.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 require.context('./assets/images', true)
 import GameManager from './core/game-manager'
 import Player from './core/player'
+import { Bump } from './core/bump'
+import * as pixi from 'pixi.js'
+
+const b = new Bump(pixi)
+console.log(b)
 
 GameManager.setup().then(() => {
   const player = new Player()
